07ccf5bd6ce916a85010eac6397a11974f249786
Rob Van Mieghem <rob@vanmieghemcloud.com>
2015-12-08 15:56:06 +0100

    build skydns ourselves

--- 6_ovc_supermaster/services/3_skydns/skydns.sh
+++ 6_ovc_supermaster/services/3_skydns/skydns.sh
@@ -2,11 +2,27 @@ set -e
 source /code/buildconfig
 set -x
 
-curl -L https://git.aydo.com/binary/skydns/raw/master/skydns -o /usr/bin/skydns
-chmod a+x /usr/bin/skydns
+
+
+ORG_PATH="github.com/skynetservices"
+REPO_PATH="${ORG_PATH}/skydns"
+
+export GOPATH=/tmp/skydnsdgopath
+
+if [ ! -d $GOPATH ]; then
+    mkdir -p $GOPATH
+fi
+
 
 mkdir -p /build/skydns
-cp /usr/bin/skydns /build/skydns/
 
-#mkdir /etc/service/skydns
-#cp /code/services/skydns/skydns.runit /etc/service/skydns/run
+go get -d -u $REPO_PATH
+
+cd $GOPATH/src/$REPO_PATH
+git checkout 2.5.3a
+
+go get -d .
+
+CGO_ENABLED=0 go build -a -installsuffix cgo -ldflags "-s" -o /build/skydns/skydns .
+
+cp /code/services/3_skydns/Dockerfile /build/skydns
