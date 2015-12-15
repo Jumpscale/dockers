85c85f992a6bbbbcf05550880d972eaa543e8799
Maxime Daniel <root@maxux.net>
2015-12-15 13:26:02 +0100

    fix missing ays init script

--- 41_ubuntu1510_js7/js7.sh
+++ 41_ubuntu1510_js7/js7.sh
@@ -3,6 +3,7 @@ set -e
 source /bd_build/buildconfig
 set -x
 
+mv /bd_build/ays /etc/init.d/ays
 export SANDBOX=0
 cd /tmp;rm -f install.sh;curl -k https://raw.githubusercontent.com/Jumpscale/jumpscale_core7/master/install/install.sh > install.sh;bash install.sh
 
