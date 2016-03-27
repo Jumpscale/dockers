86641d6d6dcb270f97e66c7585ae702023f3f644
Nourhan Raafat Omar <omarn@codescalers.com>
2016-03-27 14:15:09 +0200

    Adding build bash files

---
+++ js8/x86_64/7_ubuntu1510_buildbox/redis.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+source /bd_build/buildconfig
+set -x
+
+# groupadd -r redis && useradd -r -g redis redis
+
+# $minimal_apt_get_install build-essential
+
+mkdir -p /opt/redis
+cd /opt/redis
+wget http://download.redis.io/releases/redis-3.0.6.tar.gz
+tar xzf redis-3.0.6.tar.gz
+cd redis-3.0.6
+make
+
+# rm -rf /build/opt/redis
+# mkdir -p /build/opt/redis
+# rm -rf /build/optvar/redis/
+# mkdir -p /build/optvar/redis/
+
+rm -f /usr/local/bin/redis-server
+rm -f /usr/local/bin/redis-cli
+cp /opt/redis/redis-3.0.6/src/redis-server /opt/jumpscale8/bin/
+cp /opt/redis/redis-3.0.6/src/redis-cli /opt/jumpscale8/bin/
+
+mkdir -p /optvar/cfg/
+cp /bd_build/redis.conf /optvar/cfg/
+
+rm -rf /opt/redis
