b2d27d2fb3e0935136e3402b6a968f108b2d4ede
Nourhan Raafat Omar <omarn@codescalers.com>
2016-03-28 16:40:58 +0200

    apt-get update

--- js8/x86_64/4_ubuntu1510_js8/build.sh
+++ js8/x86_64/4_ubuntu1510_js8/build.sh
@@ -3,6 +3,8 @@ set -e
 source /bd_build/buildconfig
 set -x
 
+apt-get update
 $minimal_apt_get_install curl
+
 export SANDBOX=0
 cd /tmp;rm -f install.sh;curl -k https://raw.githubusercontent.com/Jumpscale/jumpscale_core8/master/install/install.sh > install.sh;bash install.sh
