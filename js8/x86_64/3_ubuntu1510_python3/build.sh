d5bcab6e384e8143ede1d0df183f543c6650a46e
Nourhan Raafat Omar <omarn@codescalers.com>
2016-03-28 12:30:48 +0200

    installing git

--- js8/x86_64/3_ubuntu1510_python3/build.sh
+++ js8/x86_64/3_ubuntu1510_python3/build.sh
@@ -8,6 +8,7 @@ apt-get update
 $minimal_apt_get_install libpython3.5-dev python3.5-dev libffi-dev gcc build-essential autoconf libtool pkg-config libpq-dev
 $minimal_apt_get_install libsqlite3-dev
 $minimal_apt_get_install net-tools sudo
+$minimal_apt_get_install git
 
 cd /tmp
 sudo rm -rf brotli/
