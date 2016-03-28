28b89df8c000eb81dff47c7dce1cb9d5eb661594
Nourhan Raafat Omar <omarn@codescalers.com>
2016-03-28 16:19:43 +0200

    installing curl in jumpscale step

--- js8/x86_64/4_ubuntu1510_js8/build.sh
+++ js8/x86_64/4_ubuntu1510_js8/build.sh
@@ -3,5 +3,6 @@ set -e
 source /bd_build/buildconfig
 set -x
 
+$minimal_apt_get_install curl
 export SANDBOX=0
 cd /tmp;rm -f install.sh;curl -k https://raw.githubusercontent.com/Jumpscale/jumpscale_core8/master/install/install.sh > install.sh;bash install.sh
