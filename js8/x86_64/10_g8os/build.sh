531d070a26cb48a81f77ee9ca91f292abd0a51d7
Nourhan Raafat Omar <omarn@codescalers.com>
2016-04-03 11:40:00 +0200

    upgrading pip

--- js8/x86_64/10_g8os/build.sh
+++ js8/x86_64/10_g8os/build.sh
@@ -5,7 +5,14 @@ set -x
 
 apt-get update
 
-$minimal_apt_get_install git
+$minimal_apt_get_install libpython3.5-dev python3.5-dev
+$minimal_apt_get_install wget
 
+cd /tmp
+rm -rf get-pip.py
+wget --no-check-certificate https://bootstrap.pypa.io/get-pip.py
+python3.5 get-pip.py
 
-git clone https://github.com/g8os/builder/archive/master.git
+pip install --upgrade pip
+
+pip install https://github.com/g8os/builder/archive/master.zip
