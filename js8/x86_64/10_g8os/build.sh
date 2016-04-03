4b611509af5d58dde35078df3473e6b6d2896996
Nourhan Raafat Omar <omarn@codescalers.com>
2016-04-03 13:56:50 +0200

    installing docker

--- js8/x86_64/10_g8os/build.sh
+++ js8/x86_64/10_g8os/build.sh
@@ -5,7 +5,7 @@ set -x
 
 apt-get update
 
-$minimal_apt_get_install git make sudo
+$minimal_apt_get_install git make sudo docker
 
 git config --global http.sslVerify false
 git clone https://github.com/g8os/builder.git -v
