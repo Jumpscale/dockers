d152105c8d4f59af288b3627adc7ee1ec1ef4de2
Nourhan Raafat Omar <omarn@codescalers.com>
2016-04-03 13:27:00 +0200

    installing sudo

--- js8/x86_64/10_g8os/build.sh
+++ js8/x86_64/10_g8os/build.sh
@@ -5,7 +5,7 @@ set -x
 
 apt-get update
 
-$minimal_apt_get_install git make
+$minimal_apt_get_install git make sudo
 
 git config --global http.sslVerify false
 git clone https://github.com/g8os/builder.git -v
