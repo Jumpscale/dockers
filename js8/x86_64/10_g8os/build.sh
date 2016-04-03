c5d47d35034fd5e829e50786ab22d3cc5c3acc76
Nourhan Raafat Omar <omarn@codescalers.com>
2016-04-03 12:50:31 +0200

    git cloning repo instead of pip installing

--- js8/x86_64/10_g8os/build.sh
+++ js8/x86_64/10_g8os/build.sh
@@ -5,14 +5,6 @@ set -x
 
 apt-get update
 
-$minimal_apt_get_install libpython3.5-dev python3.5-dev
-$minimal_apt_get_install wget
+$minimal_apt_get_install git
 
-cd /tmp
-rm -rf get-pip.py
-wget --no-check-certificate https://bootstrap.pypa.io/get-pip.py
-python3.5 get-pip.py
-
-pip install --upgrade pip
-
-pip install https://github.com/g8os/builder/archive/master.zip
+git clone https://github.com/g8os/builder/archive/master.zip
