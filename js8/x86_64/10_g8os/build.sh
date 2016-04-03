3ec75a36b68350b51ef3ba470c4fac85a2da592d
Nourhan Raafat Omar <omarn@codescalers.com>
2016-04-03 11:28:19 +0200

    installing using git not pip

--- js8/x86_64/10_g8os/build.sh
+++ js8/x86_64/10_g8os/build.sh
@@ -5,13 +5,7 @@ set -x
 
 apt-get update
 
-$minimal_apt_get_install libpython3.5-dev python3.5-dev libffi-dev gcc build-essential autoconf libtool pkg-config libpq-dev
-$minimal_apt_get_install wget
-
-cd /tmp
-rm -rf get-pip.py
-wget --no-check-certificate https://bootstrap.pypa.io/get-pip.py
-python3.5 get-pip.py
+$minimal_apt_get_install git
 
 
-pip install https://github.com/g8os/builder/archive/master.zip
+git clone https://github.com/g8os/builder/archive/master.git
