a0183d456af67a9ea54c47ad7ac8680701e5b6a0
Nourhan Raafat Omar <omarn@codescalers.com>
2016-04-03 13:15:28 +0200

    installing make

--- js8/x86_64/10_g8os/build.sh
+++ js8/x86_64/10_g8os/build.sh
@@ -5,7 +5,7 @@ set -x
 
 apt-get update
 
-$minimal_apt_get_install git
+$minimal_apt_get_install git make
 
 git config --global http.sslVerify false
 git clone https://github.com/g8os/builder.git -v
