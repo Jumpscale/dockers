623a1164f53974b678e1fec38875c049959768b3
Nourhan Raafat Omar <omarn@codescalers.com>
2016-04-03 11:22:04 +0200

    Trial

--- js8/x86_64/10_g8os/build.sh
+++ js8/x86_64/10_g8os/build.sh
@@ -3,10 +3,12 @@ set -e
 source /bd_build/buildconfig
 set -x
 
-$minimal_apt_get_install wget python3.5-dev 
-
 apt-get update
 
+$minimal_apt_get_install libpython3.5-dev python3.5-dev libffi-dev gcc build-essential autoconf libtool pkg-config libpq-dev
+$minimal_apt_get_install wget
+
+cd /tmp
 rm -rf get-pip.py
 wget --no-check-certificate https://bootstrap.pypa.io/get-pip.py
 python3.5 get-pip.py
