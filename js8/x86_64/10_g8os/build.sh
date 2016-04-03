4997132e8fbf248ea3835161c77889d581bffb38
Nourhan Raafat Omar <omarn@codescalers.com>
2016-04-03 10:58:58 +0200

    Edit

--- js8/x86_64/10_g8os/build.sh
+++ js8/x86_64/10_g8os/build.sh
@@ -3,10 +3,11 @@ set -e
 source /bd_build/buildconfig
 set -x
 
-$minimal_apt_get_install wget
+$minimal_apt_get_install wget python3.5
 
 rm -rf get-pip.py
-wget https://bootstrap.pypa.io/get-pip.py
+wget --no-check-certificate https://bootstrap.pypa.io/get-pip.py
+python3.5 get-pip.py
 
 apt-get update
 
