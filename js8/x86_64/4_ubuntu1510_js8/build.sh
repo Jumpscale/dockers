3df835801c7f3b5e15f9ee262b8849676202cbca
Nourhan Raafat Omar <omarn@codescalers.com>
2016-04-03 16:54:32 +0200

    installing colorlog

--- js8/x86_64/4_ubuntu1510_js8/build.sh
+++ js8/x86_64/4_ubuntu1510_js8/build.sh
@@ -5,6 +5,7 @@ set -x
 
 apt-get update
 $minimal_apt_get_install curl
+pip install colorlog
 
 export SANDBOX=0
 cd /tmp;rm -f install.sh;curl -k https://raw.githubusercontent.com/Jumpscale/jumpscale_core8/master/install/install.sh > install.sh;bash install.sh
