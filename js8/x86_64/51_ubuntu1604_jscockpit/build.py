73c4ce64d555419c1593114010aa7f5227c6bb7d
Mohamed Elawadi <mohamed@elawadi.net>
2016-06-27 11:21:10 +0200

    fix jscockpit builder.py

--- js8/x86_64/51_ubuntu1604_jscockpit/build.py
+++ js8/x86_64/51_ubuntu1604_jscockpit/build.py
@@ -1,64 +1,12 @@
 from JumpScale import j
 
-j.do.createDir("/tmp/build")
-vols='/bd_build:%s#/build:/tmp/build'%j.sal.fs.getcwd()
-print (vols)
-
-# d=j.sal.docker.create(name='build', ports='', vols=vols, volsro='', stdout=True, base='jumpscale/ubuntu1604_golang', nameserver=['8.8.8.8'],replace=True, cpu=None, mem=0, jumpscale=False, ssh=True, myinit=True, sharecode=False)
-
-
-name="g8cockpit"
-
-
 def printInfo(msg):
     print (msg)
 
-def update():
-    """
-    Update the git repo used during installation of the cockpit.
-    Run that before doing install to be sure to have last code.
-    """
-    printInfo("Update required git repository to last version")
-    repos = [
-        'https://github.com/Jumpscale/ays_jumpscale8.git',
-        'https://github.com/Jumpscale/jumpscale_core8.git',
-        'https://github.com/JumpScale/jscockpit.git'
-    ]
-    for url in repos:
-        j.do.pullGitRepo(url=url, executor=d.cuisine.executor,ssh=False)
+printInfo("Update required git repository to last version")
 
+dest = j.do.pullGitRepo(url='https://github.com/JumpScale/jscockpit.git', ssh=False)
 
-update()
+printInfo("Build docker image")
 
-"""
-Build g8cockpit docker image
-"""
-
-printInfo("create builder cuisine")
-
-cuisine = d.cuisine
-
-cuisine.package.mdupdate()
-
-cuisine.apps.portal.install(start=False)
-cuisine.apps.mongodb.build(start=False)
-cuisine.apps.influxdb.build(start=False)
-cuisine.apps.grafana.build(start=False)
-cuisine.core.run("js 'j.actions.resetAll()'")  # FIXME find why if we don't reset action before installing controller, everything explode
-cuisine.apps.controller.build(start=False)
-cuisine.apps.caddy.build(start=False)
-cuisine.apps.cockpit.build(start=False)
-cuisine.package.install('shellinabox')
-bin_path = cuisine.bash.cmdGetPath('shellinaboxd')
-cuisine.core.file_copy(bin_path, "$binDir")
-
-printInfo('clean before creating image')
-cuisine.core.dir_remove("$goDir/src/*")
-cuisine.core.dir_remove("$tmpDir/*")
-cuisine.core.dir_remove("$varDir/data/*")
-
-d.commit("jumpscale/%s" % name, delete=True, force=True)
-
-
-
-
+j.tools.cuisine.local.core.run("""cd %s;python scripts/building.py --nopush"""%dest)
