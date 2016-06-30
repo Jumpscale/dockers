48b9fa1012a8443ec74f2ebf28efcf749a3a0819
christophe de Carvalho Pereira Martincs <christophe.dcpm@gmail.com>
2016-06-30 10:16:09 +0200

    add ovs volumedirver build docker

---
+++ js8/x86_64/33_ubuntu1604_volumedriver/build.py
@@ -0,0 +1,20 @@
+from JumpScale import j
+
+name = "ubuntu1604_volumedriver"
+
+docker = j.sal.docker.create(name='build_' + name,
+                             stdout=True,
+                             base='jumpscale/ubuntu1604_arakoon',
+                             nameserver=['8.8.8.8'],
+                             replace=True,
+                             ssh=True,
+                             myinit=True,
+                             detach=True,
+                             privileged=True)
+
+docker.cuisine.apps.volumedriver.build(start=False)
+# clean source file
+d.cuisine.core.dir_remove('$tmpDir')
+d.cuisine.core.dir_ensure('$tmpDir')
+
+d.commit("jumpscale/%s" % name, delete=True, force=True)
