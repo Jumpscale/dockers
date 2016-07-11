90bc8d76f3abd6409282ed4e61b89e3c18991bed
Mohamed I. Elawadi <mielawadi@gmail.com>
2016-07-11 09:15:02 +0200

    build g8os docker using cuisine (#27)
    * build g8os docker using cuisine
    
    * pep8

--- js8/x86_64/9_g8os/build.py
+++ js8/x86_64/9_g8os/build.py
@@ -1,12 +1,79 @@
 from JumpScale import j
 
-def printInfo(msg):
-    print (msg)
+name = "g8os"
 
-printInfo("Update required git repository to last version")
 
-dest = j.do.pullGitRepo(url='https://github.com/g8os/builder_ubuntu_i64.git', ssh=False)
+d = j.sal.docker.create(
+    name='build',
+    ports='',
+    stdout=True,
+    base='jumpscale/ubuntu1604_golang',
+    nameserver=['8.8.8.8'],
+    replace=True,
+    cpu=None,
+    mem=0,
+    ssh=True,
+    sharecode=False)
 
-printInfo("Build docker image")
+cuisine = d.cuisine
 
-j.tools.cuisine.local.core.run("""cd %s;make docker"""%dest)
+cuisine.apps.core.build()
+# now, package core
+
+cuisine.core.dir_ensure('/usr/bin/')
+cuisine.core.dir_ensure('/etc/g8os/g8os.d/')
+cuisine.core.dir_ensure('/usr/lib/g8os/')
+cuisine.core.dir_ensure('/var/log/g8os/')
+
+# binaries
+cuisine.core.file_copy('$binDir/core', '/usr/bin/')
+
+sourcepath = "$goDir/src/github.com/g8os/core"
+
+cuisine.core.file_copy('%s/init' % (sourcepath), '/usr/bin/init')
+cuisine.core.run('chmod a+x /usr/bin/init')
+
+# configurations
+cuisine.core.file_copy(
+    '%s/{g8os.toml,network.toml}' %
+    (sourcepath), '/etc/g8os/')
+cuisine.core.file_copy('%s/conf/*' % (sourcepath), '/etc/g8os/g8os.d/')
+
+cuisine.core.file_copy(
+    '%s/conf.extra/*-ubuntu.toml' %
+    (sourcepath), '/etc/g8os/g8os.d/')
+
+# extensions
+cuisine.core.file_copy(
+    '%s/extensions' %
+    (sourcepath),
+    '/usr/lib/g8os/',
+    recursive=True)
+cuisine.core.dir_ensure('/usr/lib/g8os/extensions/syncthing')
+cuisine.core.file_copy(
+    '$binDir/syncthing',
+    '/usr/lib/g8os/extensions/syncthing')
+
+# corectl
+cuisine.golang.clean_src_path()
+cuisine.golang.get('github.com/g8os/corectl')
+cuisine.core.file_copy('$goDir/bin/corectl', '/usr/bin/corectl')
+
+cuisine.core.file_unlink(
+    '/etc/g8os/g8os.d/{agetty-ubuntu.toml,modprobe.toml,udev-ubuntu.toml,basic.jumpscripts.toml}')
+cuisine.core.run("perl -i -pe 's/^.*network\.toml.*$//' /etc/g8os/g8os.toml")
+
+d.commit(
+    "jumpscale/%s" %
+    name,
+    delete=True,
+    force=True,
+    conf={
+        'CMD': [
+            '/usr/bin/core',
+            '-nid',
+            '1',
+            '-gid',
+            '1',
+            '-roles',
+            'g8os']})
