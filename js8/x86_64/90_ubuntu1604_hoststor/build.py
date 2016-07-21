711ec81374b0bd1ae7988436bb5eb73edce828ce
Muhamad Azmy <muhamad.azmy@codescalers.com>
2016-07-21 11:49:11 +0200

    Building stor

--- js8/x86_64/90_ubuntu1604_hoststor/build.py
+++ js8/x86_64/90_ubuntu1604_hoststor/build.py
@@ -2,6 +2,8 @@ from JumpScale import j
 
 j.actions.resetAll()
 
+namespace = 'main'
+
 logger = j.logger.get('j.docker.sandbox_upload')
 d = j.sal.docker.create(name='js8_host',
                         stdout=True,
@@ -13,50 +15,36 @@ d = j.sal.docker.create(name='js8_host',
                         ports="22:7022 8090:8090",
                         sharecode=False,
                         setrootrndpasswd=False,
-                        vols="/out:/storage/builder/sandbox_ub1604")
+                        vols="/mnt/aydostorx/namespaces/{namespace}:/storage/builder/sandbox_ub1604/js8/files".format(namespace=namespace))
 
-CONFIG="""
-listen_addr="localhost:8090"
-store_root="/mnt/openvcloud/aydostorx"
-#sources=["http://localhost:8081"]
-#authentification="auth.toml"
-#[tls]
-#certificate="cert.pem"
-#key="key.rsa"
+CONFIG = """
+listen_addr = "localhost:8090"
+store_root = "/mnt/aydostorx/"
 """
 
-d.cuisine.core.dir_ensure("/out/aydostorx/fixed")
-d.cuisine.core.dir_ensure("/out/aydostorx/namespaces")
-d.cuisine.core.dir_ensure("/out/aydostorx/tmp")
+d.cuisine.core.dir_ensure("/mnt/aydostorx/fixed")
+d.cuisine.core.dir_ensure("/mnt/aydostorx/tmp")
 
-d.cuisine.core.file_write("/etc/js_storx.toml",CONFIG)
+d.cuisine.core.file_write("/etc/js_storx.toml", CONFIG)
 
-#can be removed is already in sandbox build
-add='source /opt/jumpscale8/env.sh'
-prof=d.cuisine.core.file_read("/root/.profile")
-
-if not add in prof:
-    prof+='\n%s\n'%add
-    d.cuisine.core.file_write("/root/.profile",prof)
-
-
-pm=d.cuisine.processmanager.get(pm="tmux")
+pm = d.cuisine.processmanager.get(pm="tmux")
 
 cmd="./stor -c /etc/js_storx.toml"
 pm.ensure(name="storx", cmd=cmd, env={}, path='/opt/jumpscale8/bin', descr='')
 
-if not j.sal.nettools.tcpPortConnectionTest("localhost",8090):
+if not j.sal.nettools.tcpPortConnectionTest("localhost", 8090):
     raise RuntimeError("cannot connect over tcp to port 8090 on localhost")
 
-store_client = j.clients.storx.get('http://localhost:8090')
-
-#@todo PLEASE COMPLETE
-
-from IPython import embed
-print ("DEBUG NOW sdsd")
-embed()
-p
-
-#do a test
-store_client.putFile("test","/storage/builder/sandbox_ub1604/js8/files/a/a/aa6a634cf373728e934b3d6da15759bd.bro")
-
+#
+# store_client = j.clients.storx.get('http://localhost:8090')
+#
+# #@todo PLEASE COMPLETE
+#
+# from IPython import embed
+# print ("DEBUG NOW sdsd")
+# embed()
+# p
+#
+# #do a test
+# store_client.putFile("test","/storage/builder/sandbox_ub1604/js8/files/a/a/aa6a634cf373728e934b3d6da15759bd.bro")
+#
