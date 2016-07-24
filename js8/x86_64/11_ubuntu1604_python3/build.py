60ec10374b8e90217496b88de6b0455136556851
Abdulrahman tarek khalifa <tareka@codescalers.com>
2016-07-24 10:29:30 +0200

    removed embedded shell , fixed typos
    i tried to build it , it works i dont know why the shell was needed

--- js8/x86_64/11_ubuntu1604_python3/build.py
+++ js8/x86_64/11_ubuntu1604_python3/build.py
@@ -24,19 +24,12 @@ d = j.sal.docker.create(name='build',
 
 
 d.cuisine.installer.base()
-
-from IPython import embed
-print ("DEBUG NOW sdsds")
-embed()
-p
-
-
 d.cuisine.installerdevelop.python()
 d.cuisine.installerdevelop.pip()
 d.cuisine.installerdevelop.installJS8Deps()
 
 # make sure brotli is installed
-if not d1.cuisine.core.command_check('bro'):
+if not d.cuisine.core.command_check('bro'):
     bro_script = """
     cd $tmpDir; git clone https://github.com/google/brotli.git
     cd $tmpDir/brotli/
@@ -44,7 +37,7 @@ if not d1.cuisine.core.command_check('bro'):
     make bro
     cp bin/bro $binDir/bro
     """
-    d1.cuisine.core.run_script(bro_script)
+    d.cuisine.core.run_script(bro_script)
 
 d.cuisine.installerdevelop.cleanup()
 
